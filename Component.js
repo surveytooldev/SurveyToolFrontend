e900f3fcc983b54ff4d3c3025635b6ddc4e241fc
Anika Schmidt <anika.schmidt@sap.com>
2019-07-25 10:29:13 +0200

    routing enabled

--- Component.js
+++ Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
-	'sap/ui/model/json/JSONModel'
-], function(UIComponent, JSONModel) {
+	'sap/ui/model/json/JSONModel',
+	'sap/f/library'
+], function(UIComponent, JSONModel, fioriLibrary) {
 	'use strict';
 
 	return UIComponent.extend('sap.ui.demo.fiori2.Component', {
@@ -11,15 +12,35 @@ sap.ui.define([
 		},
 
 		init: function () {
-			var oProductsModel;
+			var oModel,
+				oProductsModel,
+				oRouter;
 
 			UIComponent.prototype.init.apply(this, arguments);
 
+			oModel = new JSONModel();
+			this.setModel(oModel);
+
 			// set products demo model on this sample
-		/* 	oProductsModel = new JSONModel("../mock.json");
+			oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json');
 			oProductsModel.setSizeLimit(1000);
-			print(oProductsModel);
-			this.setModel(oProductsModel, 'feedbackListItems'); */
+			this.setModel(oProductsModel, 'products');
+
+			oRouter = this.getRouter();
+			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+			oRouter.initialize();
+		},
+
+		_onBeforeRouteMatched: function(oEvent) {
+			var oModel = this.getModel(),
+				sLayout = oEvent.getParameters().arguments.layout;
+
+			// If there is no layout parameter, set a default layout (normally OneColumn)
+			if (!sLayout) {
+				sLayout = fioriLibrary.LayoutType.OneColumn;
+			}
+
+			oModel.setProperty("/layout", sLayout);
 		}
 	});
 });
