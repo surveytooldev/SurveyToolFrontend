63daec80c73d4d6fd0169f3c42f7a178ca5937cc
Anika Schmidt <anika.schmidt@sap.com>
2019-07-26 12:30:55 +0200

    detai controler

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -8,6 +8,25 @@ sap.ui.define([
             oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function (oEvent) {
+            $.get("http://ip-api.com/json/24.48.0.1", function(data){
+                console.log(data);
+            });
+            //$.post("https://survey-tool-backend.herokuapp.com/survey/token/", {username: "admin", password:"admin"}).done(function(data){console.log(data)})
+            
+            $.ajax({
+                type: 'POST',
+                url: 'https://survey-tool-backend.herokuapp.com/survey/token/',
+                crossDomain: true,
+                data: '{"username": "admin", "password":"admin"}',
+                dataType: 'json',
+                success: function(responseData, textStatus, jqXHR) {
+                   console.log(responseData)
+                },
+                error: function (responseData, textStatus, errorThrown) {
+                    alert('POST failed.');
+                }
+            });
+            
             console.log(oEvent.getParameter("arguments").actionitem);
 		}
     });
