71fd15da6fb99e4ae671bd63ae306588f25129fb
Anika Schmidt <anika.schmidt@sap.com>
2019-08-12 23:59:24 +0200

    finalizing user management

--- controller/Master.controller.js
+++ controller/Master.controller.js
@@ -14,6 +14,18 @@ sap.ui.define([
 	return Controller.extend("sap.surveytool.controller.Master", {
 
 		onInit: function () {
+			var oModel = new JSONModel();
+			oModel.setData(
+				{
+					"user_status": [
+						{ "name": "Active" },
+						{ "name": "Inactive" },
+						{ "name": "All" }
+					]
+				});
+			this.getView().setModel(oModel);
+
+			this.getView().byId("userButton").setEnabled(false);
 			this.oRouter = this.getOwnerComponent().getRouter();
 			this.oRouter.getRoute("master").attachPatternMatched(this._onObjectMatched, this);
 			if (sessionStorage.getItem("accessToken") == null) {
@@ -34,21 +46,23 @@ sap.ui.define([
 		},
 
 		onBeforeRendering: function () {
-
 			if (sessionStorage.getItem("accessToken") != null) {
 				this.instantiateModels();
 			}
-			if(sessionStorage.getItem("userGroup") == "lob"){
+			if (sessionStorage.getItem("userGroup") == "lob") {
 				this.getView().byId("lobButton").setEnabled(false);
 				this.getView().byId("serviceButton").setEnabled(false);
 				this.getView().byId("topicButton").setEnabled(false);
 				this.getView().byId("statusButton").setEnabled(false);
 				this.getView().byId("addButton").setEnabled(false);
 			}
+			if (sessionStorage.getItem("userGroup") == "admin") {
+				this.getView().byId("userButton").setEnabled(true);
+			}
 		},
 
-		_onObjectMatched: function()	{
-			
+		_onObjectMatched: function () {
+
 		},
 
 		onRowPress: function (oEvent) {
@@ -137,6 +151,7 @@ sap.ui.define([
 		},
 
 		postData: function (url, data) {
+			var t = this;
 			$.ajax({
 				url: "https://survey-tool-backend.herokuapp.com/survey/".concat(url),
 				type: 'POST',
@@ -145,7 +160,7 @@ sap.ui.define([
 					'Authorization': "Bearer  ".concat(sessionStorage.getItem("accessToken"))
 				},
 				contentType: 'application/json; charset=utf-8',
-				data: 
+				data:
 					data
 				,
 				success: function (data) {
@@ -186,6 +201,7 @@ sap.ui.define([
 					}
 				});
 			}
+			console.log(e);
 		},
 
 		//view-related functions must be excluded
@@ -203,22 +219,82 @@ sap.ui.define([
 			} else {
 				this.byId(nameForId).open();
 			}
-		
+
 		},
 		onCloseDialogGeneric: function (dialogId) {
 			this.byId(dialogId).close();
 		},
-		onAddItemPress: function(url)	{
+		onAddItemPress: function (url) {
 			this.onOpenDialogGeneric("dialog.ItemDialog", "ItemDialog");
 			this.byId("ItemDialog").setTitle(url);
 		},
 
-		onSaveItem: function()	{
+		onSaveItem: function () {
 			var _name = this.getView().byId("itemName").getValue();
 			var _desc = this.getView().byId("itemDescription").getValue();
-			var json = { name : _name , description : _desc};
+			var json = { name: _name, description: _desc };
 			var data = JSON.stringify(json);
 			this.postData(this.byId("ItemDialog").getTitle(), data);
+		},
+
+		onShowUsers: function () {
+			var status = this.getView().byId("user_status").getSelectedKey().toLowerCase();
+			this.loadData("users/".concat(status), "users");
+			switch (status) {
+				case "active":
+					this.getView().byId("activate").setEnabled(false);
+					this.getView().byId("deactivate").setEnabled(true);
+					this.getView().byId("delete").setEnabled(true);
+					break;
+				case "inactive":
+					this.getView().byId("activate").setEnabled(true);
+					this.getView().byId("deactivate").setEnabled(false);
+					this.getView().byId("delete").setEnabled(true);
+					break;
+				case "all":
+					this.getView().byId("activate").setEnabled(true);
+					this.getView().byId("deactivate").setEnabled(true);
+					this.getView().byId("delete").setEnabled(true);
+					break;
+			}
+
+		},
+
+		onChangeUserStatus: function (status) {
+			var selectedUser = this.getView().byId("userList").getSelectedKey();
+			var t = this;
+			switch (status) {
+				case "activate":
+					var json = { action: "activate" };
+					this.postData("userdetail/".concat(selectedUser), JSON.stringify(json));
+					MessageToast.show("Activated User");
+					this.onShowUsers();
+					break;
+				case "deactivate":
+					var json = { action: "deactivate" };
+					this.postData("userdetail/".concat(selectedUser), JSON.stringify(json));
+					MessageToast.show("Deactivated User");
+					this.onShowUsers();
+					break;
+				case "delete":
+					$.ajax({
+						url: "https://survey-tool-backend.herokuapp.com/survey/".concat("userdetail/").concat(selectedUser),
+						type: 'DELETE',
+						dataType: 'json',
+						headers: {
+							'Authorization': "Bearer  ".concat(sessionStorage.getItem("accessToken"))
+						},
+						contentType: 'application/json; charset=utf-8',
+						success: function (data) {
+							t.MessageToast.show("Deleted successfully");
+						},
+						error: function (e) {
+							console.log(e);
+						}
+					});
+					this.onShowUsers();
+					break;
+			}
 		},
 
 		onViewList: function (url, nameOfModel, pathToFragment, nameForId) {
@@ -231,7 +307,7 @@ sap.ui.define([
 		onDeleteService: function () {
 			var itemText = this.getView().byId("serviceList").getSelectedItem().getText();
 		},
-		onLogout: function()	{
+		onLogout: function () {
 			sessionStorage.clear();
 			location.reload();
 		}
